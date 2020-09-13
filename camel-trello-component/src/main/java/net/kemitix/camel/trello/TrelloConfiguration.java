ca819c71d4a35351b56e8547d51ee8f0db5d1d5b
Paul Campbell <pcampbell@kemitix.net>
2020-09-13 10:41:01 +0100

    Wipe initial import

--- camel-trello-component/src/main/java/net/kemitix/camel/trello/TrelloConfiguration.java
+++
@@ -1,9 +0,0 @@
-package net.kemitix.camel.trello;
-
-import org.apache.camel.spi.UriParams;
-
-@UriParams
-public class TrelloConfiguration {
-
-    // TODO add component configuration properties
-}
