380358a64d7e56a13ff6833c03b6442890af6239
Paul Campbell <pcampbell@kemitix.net>
2020-09-13 18:46:46 +0100

    refactoring

--- src/main/java/net/kemitix/camel/trello/TrelloComponent.java
+++ src/main/java/net/kemitix/camel/trello/TrelloComponent.java
@@ -6,9 +6,10 @@ import com.julienvey.trello.Trello;
 import org.apache.camel.Endpoint;
 
 import org.apache.camel.spi.Metadata;
+import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
 
-@org.apache.camel.spi.annotations.Component("trello")
+@Component("trello")
 public class TrelloComponent extends DefaultComponent {
 
     @Metadata
