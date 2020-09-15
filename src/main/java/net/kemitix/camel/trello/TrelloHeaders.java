9a3e6b1c265ff7cc4d29231ac65c763676a59a9a
Paul Campbell <pcampbell@kemitix.net>
2020-09-15 19:16:49 +0100

    TrelloHeaders: added

---
+++ src/main/java/net/kemitix/camel/trello/TrelloHeaders.java
@@ -0,0 +1,7 @@
+package net.kemitix.camel.trello;
+
+public class TrelloHeaders {
+    public static final String ACTION = "CamelTrello.action";
+    public static final String BOARD = "CamelTrello.board";
+    public static final String LIST = "CamelTrello.list";
+}
