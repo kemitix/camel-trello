a64c19e938354abf34df1836942dc90b9e0f5df4
Paul Campbell <pcampbell@kemitix.net>
2020-09-16 08:59:52 +0100

    ListGetCards: add failing test for missing board name and id

--- src/test/java/net/kemitix/camel/trello/ListGetCardsTest.java
+++ src/test/java/net/kemitix/camel/trello/ListGetCardsTest.java
@@ -3,6 +3,7 @@ package net.kemitix.camel.trello;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
+import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,10 +14,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-public class ListGetCardsTest extends CamelTestSupport {
+public class ListGetCardsTest extends CamelTestSupport
+        implements WithAssertions {
 
     public static final String BOARD_ID = "Board Id";
     public static final String LIST_ID = "List Id";
@@ -28,6 +31,7 @@ public class ListGetCardsTest extends CamelTestSupport {
 
     Object body = "";
     Map<String, Object> headers = new HashMap<>();
+    Exception e;
 
     @Test
     @DisplayName("valid with board name and list name")
@@ -46,7 +50,8 @@ public class ListGetCardsTest extends CamelTestSupport {
         verify(trelloService).listGetCards(BOARD_ID, LIST_ID);
     }
 
-    @Test @DisplayName("valid with board id and list name")
+    @Test
+    @DisplayName("valid with board id and list name")
     public void validIdName() throws Exception {
         //given
         MockEndpoint mock = getMockEndpoint("mock:result");
@@ -61,7 +66,8 @@ public class ListGetCardsTest extends CamelTestSupport {
         verify(trelloService).listGetCards(BOARD_ID, LIST_ID);
     }
 
-    @Test @DisplayName("valid with board name and list id")
+    @Test
+    @DisplayName("valid with board name and list id")
     public void validNameId() throws Exception {
         //given
         MockEndpoint mock = getMockEndpoint("mock:result");
@@ -76,7 +82,8 @@ public class ListGetCardsTest extends CamelTestSupport {
         verify(trelloService).listGetCards(BOARD_ID, LIST_ID);
     }
 
-    @Test @DisplayName("valid with board id and list id")
+    @Test
+    @DisplayName("valid with board id and list id")
     public void validIdId() throws Exception {
         //given
         MockEndpoint mock = getMockEndpoint("mock:result");
@@ -90,10 +97,24 @@ public class ListGetCardsTest extends CamelTestSupport {
         verify(trelloService).listGetCards(BOARD_ID, LIST_ID);
     }
 
+    @Test
+    @DisplayName("missing board name and id")
+    public void missingBoardNameAndId() throws Exception {
+        //given
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedMinimumMessageCount(1);
+        headers.put(TrelloHeaders.LIST_ID, LIST_ID);
+        //when
+        template.sendBodyAndHeaders("direct:start", body, headers);
+        //then
+        mock.await();
+        verify(trelloService, never()).listGetCards(BOARD_ID, LIST_ID);
+        assertThat(e).isNotNull();
+    }
 
-//    @Test @DisplayName("missing board name or id")
-//    @Test @DisplayName("missing list name or id")
-//    @Test @DisplayName("missing board name or id and list name or id")
+    //    @Test @DisplayName("missing list name and id")
+    //    @Test @DisplayName("board id not found for board name")
+    //    @Test @DisplayName("list id not found for list name")
 
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
@@ -101,6 +122,8 @@ public class ListGetCardsTest extends CamelTestSupport {
                 .bind("trelloService", TrelloService.class, trelloService);
         return new RouteBuilder() {
             public void configure() {
+                onException(IllegalArgumentException.class)
+                        .process(exchange -> e = exchange.getException());
                 from("direct:start")
                         .to("trello:test" +
                                 "?action=list_get_cards")
